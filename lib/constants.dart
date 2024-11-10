55d506cc2b5fa5492057844f7e1316cd348e5c31
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-10 19:24:00 +0530

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const bColor = Color.fromARGB(255, 30, 30, 30);
+
+const HeaderText1 = TextStyle(
+  color: Color.fromARGB(255, 255, 255, 255), 
+  fontSize: 40, 
+  fontWeight: FontWeight.bold,
+  height: 1,
+  );
+
+const BodyText1 = TextStyle(
+  color: Color.fromARGB(255, 255, 255, 255), 
+  fontSize: 15,
+  height: 1,
+  );
+
+
+  
