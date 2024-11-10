b0f3b930cb5982bb6712314cccfaacef0b035f39
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-10 19:23:53 +0530

    Create routes_screen.dart

---
+++ lib/screens/routes_screen.dart
@@ -0,0 +1,41 @@
+import 'package:canto/constants.dart';
+import 'package:canto/custom_widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+var bgImage = 'assets/images/routes_bg.png';
+
+class RoutesScreen extends StatelessWidget {
+   const RoutesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bColor,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage(bgImage), fit: BoxFit.cover),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Spacer(),
+                const Text('Welcome to\nCanteen Token ',
+                style: HeaderText1,),
+                const SizedBox(height: 20,),
+                const Text('Log in to reserve meal tokens,\nview the menu, and manage reservations.',
+                style: BodyText1,),
+                const SizedBox(height: 20,),
+                CustomButton(text: 'Sign Up', onPressed: (){}),
+                const SizedBox(height: 20,),
+                CustomButton(text: 'Login', onPressed: (){}),
+                const SizedBox(height: 25,),
+              ],  
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
