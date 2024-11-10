20c27144dc2c3051a455864b990e525b94b3c902
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-10 19:23:49 +0530

    Create splash_screen.dart

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:canto/screens/routes_screen.dart';
+import 'package:flutter/material.dart';
+
+var logo = 'assets/images/logo.png';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    void navigateToNextPage() {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => RoutesScreen(),
+        ),
+      );
+    }
+
+    Future.delayed(const Duration(seconds: 2), navigateToNextPage);
+
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 255, 255, 255)
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Image.asset(
+                  logo,
+                  width: 250,
+                  height: 250,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
