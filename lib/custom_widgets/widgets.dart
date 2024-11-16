fe4de784e9acce55ee367baefcda69c86df9fcf5
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-17 00:40:23 +0530

    Update widgets.dart

--- lib/custom_widgets/widgets.dart
+++ lib/custom_widgets/widgets.dart
@@ -99,3 +99,47 @@ class CustomTextInput extends StatelessWidget {
     );
   }
 }
+
+class CustomCard extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final VoidCallback onTap;
+
+  const CustomCard({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 5,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 60,
+              color: Colors.black, 
+            ),
+            const SizedBox(height: 10),
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
