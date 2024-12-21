3db06113bd4019aec719f0de259efc294d4dedb6
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-12-22 01:03:20 +0530

    Update notification_screen.dart

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -9,102 +9,46 @@ class NotificationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           icon:Icon(Icons.arrow_back,color: Colors.white,),
           onPressed: (){},
         ),
-        title: Text('Notifications',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),
+        title: const Text(
+          'Notifications',
+          style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),
         ),
       backgroundColor: Colors.black,
       ),
-      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 20,
-              color: Colors.black,
-            ),  
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(60),
-                  ),
-                ),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(height: 50,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text('Name',style: BodyText4,),
-                          Spacer(),
-                          Text('Uditha Thathsara',style: BodyText5,),
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-                      Divider(
-                      color: Colors.black54, 
-                      thickness: 1,
-                      ),
-                      SizedBox(height: 10,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text('date',style: BodyText4,),
-                          Spacer(),
-                          Text('30 Octomber 2024',style: BodyText5,),
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-                      Divider(
-                      color: Colors.black54, 
-                      thickness: 1,
-                      ),
-                      SizedBox(height: 10,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text('Food',style: BodyText4,),
-                          Spacer(),
-                          Text('Kottu',style: BodyText5,),
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-                      Divider(
-                      color: Colors.black54, 
-                      thickness: 1,
-                      ),
-                      SizedBox(height: 10,),  
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text('Time to Pick',style: BodyText4,),
-                          Spacer(),
-                          Text('2.30 pm',style: BodyText5,),
-                        ],
-                      ),
-                      SizedBox(height: 10,),
-                      Divider(
-                      color: Colors.black54, 
-                      thickness: 1,
-                      ),
-                      SizedBox(height: 10,),
-                    ],
-                  ),
-                )
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 30,),
+                  NotificationCard(name: "Gunasena", price: "120", date: "30th Octomber"),
+                  SizedBox(height: 30,),
+                  NotificationCard(name: "Gunasena", price: "120", date: "30th Octomber"),
+                  SizedBox(height: 30,),
+                  NotificationCard(name: "Gunasena", price: "120", date: "30th Octomber"),
+                  SizedBox(height: 30,),
+                  NotificationCard(name: "Gunasena", price: "120", date: "30th Octomber"),
+                  SizedBox(height: 30,),
+                  NotificationCard(name: "Gunasena", price: "120", date: "30th Octomber"),
+                  SizedBox(height: 30,),
+                  NotificationCard(name: "Gunasena", price: "120", date: "30th Octomber"),
+                  SizedBox(height: 30,),
+                  NotificationCard(name: "Gunasena", price: "120", date: "30th Octomber"),
+                ],
               ),
             ),
-          ],
+          )
         ),
       ),
     );
