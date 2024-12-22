9259efe0300e4be17e3edb187fba27833185973e
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-12-23 01:29:47 +0530

    Create canteen_food_edit.dart

---
+++ lib/screens/canteen_food_edit.dart
@@ -0,0 +1,162 @@
+import 'package:canto/custom_widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+
+
+class CanteenFoodEditScreen extends StatefulWidget {
+  const CanteenFoodEditScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CanteenFoodEditScreen> createState() => _CanteenFoodEditScreenState();
+}
+
+class _CanteenFoodEditScreenState extends State<CanteenFoodEditScreen> {
+
+  Future<void> openCamera() async {
+    final ImagePicker _picker =ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    if (image != null ) {
+      print('Image path: ${image.path}');
+    }
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon:Icon(Icons.arrow_back,color: Colors.white,),
+          onPressed: (){},
+      ),
+      
+      backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 20,
+              color: Colors.black,
+            ),  
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 20,),
+                    GestureDetector(
+                      onTap: openCamera,
+                      child: Container(
+                        height: 180,
+                        width: 285,
+                        color: Colors.grey.shade300,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.camera_alt,
+                              size: 30,
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      decoration: BoxDecoration(
+                        color:Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(8)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(labelText: 'Name'),
+                              onChanged: (value) {
+                              },
+                            ),
+                            TextField(
+                              decoration: InputDecoration(labelText: 'Description'),
+                              maxLines: 3,
+                              onChanged: (value) {
+                              },
+                            ),
+                            TextField(
+                              decoration: InputDecoration(labelText: 'Price'),
+                              keyboardType: TextInputType.number,
+                              onChanged: (value) {
+                              },
+                            ),
+                            
+                            
+                            SizedBox(height: 20,)
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(labelText: 'Promotion Code',
+                            border: InputBorder.none,
+                            prefixIcon: Icon(Icons.discount_rounded),
+                            ),
+                            onChanged: (value) {
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 8,),
+                        TextButton(
+                          onPressed: (){}, 
+                          style: TextButton.styleFrom(
+                            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                            minimumSize: Size(20, 20),
+                            backgroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: Text(
+                            'Apply',
+                            style: TextStyle(fontSize: 12.0, color: Colors.white),))
+                      ],
+                    ),
+
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomButton2(text: 'Save', onPressed: (){}),
+                      ],
+                    ),
+                    SizedBox(height: 10,)
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
