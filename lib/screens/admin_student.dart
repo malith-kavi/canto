e34548f8d8a3e1c3710a06ad95210557cf407658
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-12-23 01:29:38 +0530

    Create admin_student.dart

---
+++ lib/screens/admin_student.dart
@@ -0,0 +1,51 @@
+import 'package:canto/constants.dart';
+import 'package:canto/custom_widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+
+
+class AdminStudent extends StatelessWidget {
+  const AdminStudent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon:Icon(Icons.arrow_back,color: Colors.white,),
+          onPressed: (){},
+        ),
+        title: const Text(
+          'Students',
+          style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),
+        ),
+      backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 30,),
+                  AdminCard(name: 'Uditha Thathsara'),
+                  SizedBox(height: 30,),
+                  AdminCard(name: 'Uditha Thathsara'),
+                  SizedBox(height: 30,),
+                  AdminCard(name: 'Uditha Thathsara'),
+                  SizedBox(height: 30,),
+                  AdminCard(name: 'Uditha Thathsara'),
+                  
+                ],
+              ),
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
