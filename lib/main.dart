e1f495b86f47b37a9e50f54c28c7324130c9b1c9
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-12-23 01:29:19 +0530

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:canto/screens/admin_canteen_staff.dart';
+import 'package:canto/screens/admin_dashboard.dart';
+import 'package:canto/screens/admin_menu.dart';
+import 'package:canto/screens/admin_profile_screen.dart';
+import 'package:canto/screens/admin_settings_screen.dart';
+import 'package:canto/screens/admin_student.dart';
+import 'package:canto/screens/admin_token_screen.dart';
+import 'package:canto/screens/admin_token_summery_screen.dart';
 import 'package:canto/screens/canteen_dashboard.dart';
 import 'package:canto/screens/canteen_food_add.dart';
+import 'package:canto/screens/canteen_food_edit.dart';
 import 'package:canto/screens/canteen_food_screen.dart';
 import 'package:canto/screens/canteen_menu.dart';
+import 'package:canto/screens/canteen_message.dart';
+import 'package:canto/screens/canteen_profile.dart';
+import 'package:canto/screens/canteen_settings.dart';
 import 'package:canto/screens/cart_screen.dart';
+import 'package:canto/screens/feedback_screen.dart';
 import 'package:canto/screens/food_screen.dart';
 import 'package:canto/screens/login_screen.dart';
 import 'package:canto/screens/notification_screen.dart';
@@ -13,7 +26,9 @@ import 'package:canto/screens/student_dashboard.dart';
 import 'package:canto/screens/student_menu.dart';
 import 'package:canto/screens/student_profile.dart';
 import 'package:canto/screens/student_settings_screen.dart';
+import 'package:canto/screens/student_tokens_screen.dart';
 import 'package:canto/screens/token_screen.dart';
+import 'package:canto/screens/token_summery.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +47,7 @@ class MyApp extends StatelessWidget {
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
-      home: CanteenFoodAddScreen(),
+      home: AdminProfileScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
