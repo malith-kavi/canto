983429a876c67d607157e4239da4d0a82cf7bee8
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2025-01-08 01:21:40 +0530

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 import 'package:canto/constants.dart';
 import 'package:canto/custom_widgets/widgets.dart';
+import 'package:canto/screens/admin_dashboard.dart';
+import 'package:canto/screens/canteen_dashboard.dart';
+import 'package:canto/screens/student_dashboard.dart';
 import 'package:flutter/material.dart';
 
 var logo = 'assets/images/logo.png';
@@ -14,136 +17,159 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bColor,
-      body: Column(
-        children: [
-          SafeArea(
-            child: Expanded(
-              flex: 8,
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: bColor,
-                ),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(1),
-                        child: Image.asset(
-                          logo,
-                          width: 150,
-                          height: 150,
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+              Expanded(
+                flex: 8,
+                child: SafeArea(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: bColor,
+                    ),
+                    child: Center(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(1),
+                              child: Image.asset(
+                                logo,
+                                width: 150,
+                                height: 150,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
+                    ),
+                  ),
+                ),
+              ),
+            
+            SizedBox(
+              height: 20,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(43, 255, 255, 255),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(43, 255, 255, 255),
+            Expanded(
+              flex: 18,
+              child:Container(
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
                   borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25),
-                    topRight: Radius.circular(25),
-                  ),
+                      topLeft: Radius.circular(32),
+                      topRight: Radius.circular(32),
+                    ),
                 ),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 18,
-            child:Container(
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 255, 255, 255),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(32),
-                    topRight: Radius.circular(32),
-                  ),
-              ),
-              child:Center(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 15, 20, 25),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Welcome back',
-                        style: HeaderText2,
-                      ),
-                      SizedBox(height: 15,),
-                      Text('Enter your details here.',
-                      style: BodyText3,),
-                      SizedBox(height: 15,),
-                      
-                      CustomTextInput(hintText: 'Email Address'),
-                      SizedBox(height: 15,),
-                      CustomTextInput(hintText: 'Password'),
-                      Spacer(),
-                      CustomButton(text: 'Login', onPressed: (){}),
-                      SizedBox(height: 15,),
-                      Row(
+                child:Center(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 25),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          
-                          Expanded(
-                            child: Divider(
-                              color: Colors.black54, 
-                              thickness: 1.5, 
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                              'Or Login with',
-                              style: BodyText2
-                            ),
-                          ),
-                          
-                          Expanded(
-                            child: Divider(
-                              color: Colors.black54, 
-                              thickness: 1.5,
-                            ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    'Welcome back',
+                                    style: HeaderText2,
+                                  ),
+                                  SizedBox(height: 15,),
+                                  Text('Enter your details here.',
+                                  style: BodyText3,),
+                                  SizedBox(height: 15,),
+                                  
+                                  CustomTextInput(hintText: 'Email Address'),
+                                  SizedBox(height: 15,),
+                                  CustomTextInput(hintText: 'Password'),
+                                  SizedBox(height: 70,),
+                                  
+                                  CustomButton(
+                                    text: 'Login', 
+                                    onPressed: (){
+                                      Navigator.push(context, MaterialPageRoute(builder: (context)=> AdminDashboard()));
+                                    }),
+                                  SizedBox(height: 15,),
+                                  Row(
+                                    children: [
+                                      
+                                      Expanded(
+                                        child: Divider(
+                                          color: Colors.black54, 
+                                          thickness: 1.5, 
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                        child: Text(
+                                          'Or Login with',
+                                          style: BodyText2
+                                        ),
+                                      ),
+                                      
+                                      Expanded(
+                                        child: Divider(
+                                          color: Colors.black54, 
+                                          thickness: 1.5,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Spacer(),
+                                      IconButton(
+                                        icon: Image.asset(
+                                          facebook,
+                                          height: 50,
+                                          width: 50,
+                                        ),
+                                        onPressed: () {},
+                                      ),
+                                      Spacer(),
+                                      IconButton(
+                                        icon: Image.asset(
+                                          google,
+                                          height: 50,
+                                          width: 50,
+                                        ),
+                                        onPressed: () {},
+                                      ),
+                                      Spacer(),
+                                    ],
+                                  ),
+                                                    
+                                ],
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Spacer(),
-                          IconButton(
-                            icon: Image.asset(
-                              facebook,
-                              height: 50,
-                              width: 50,
-                            ),
-                            onPressed: () {},
-                          ),
-                          Spacer(),
-                          IconButton(
-                            icon: Image.asset(
-                              google,
-                              height: 50,
-                              width: 50,
-                            ),
-                            onPressed: () {},
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-
-                    ],
+                    ),
                   ),
-                ),
+              ),
+              ),
             ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
